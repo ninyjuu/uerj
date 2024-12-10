c043eaa105c0c5f3e56e927b5f8cbb1e32c0b157
ninyjuu <140440526+ninyjuu@users.noreply.github.com>
2024-12-10 15:28:45 -0300

    A

--- p2/ex?.py
+++
@@ -1,9 +0,0 @@
-#Intercalar Listas: Crie duas listas de números com o mesmo tamanho.
-#Escreva um programa que gere uma nova lista contendo os
-#elementos das duas listas intercalados (um elemento de cada lista por vez).
-
-lista1=[1,3,5,7,9]
-lista2=[0,2,4,6,8]
-for i in range(5):
-    lista= [lista1(i),lista2[i]]
-print(lista)

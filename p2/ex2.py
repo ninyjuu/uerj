dd7b85992463cfeafbd20c256775c37bc43c7768
ninyjuu <140440526+ninyjuu@users.noreply.github.com>
2024-12-12 09:00:25 -0300

    uerj

--- p2/ex2.py
+++ p2/ex2.py
@@ -2,21 +2,22 @@
 #maior número sem utilizar funções prontas do Python e imprima
 
 lista= []
-maior= 0
-smaior= 0
 
 for i in range (0,5):
     n= int(input('numero: '))
     lista.append(n)
+    
+maior= lista[0]
 
 for j in range (len(lista)):
     if lista[j]> maior:
         maior= lista[j]
-lista.remove(maior)
 print(maior)
 
+smaior= lista[0]
+
 for k in range (len(lista)):
-    if lista[k]> smaior:
+    if lista[k]!= maior and lista[k]> smaior:
         smaior = lista[k]
         
 print(smaior)

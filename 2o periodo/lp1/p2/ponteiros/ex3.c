284f1b8ffae2c3ab73fc32acbe67e3270fe7bcc6
ninyjuu <140440526+ninyjuu@users.noreply.github.com>
2025-05-30 23:38:29 -0300

    a

---
+++ 2o periodo/lp1/p2/ponteiros/ex3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void inverterVetor(int *v, int n) {
+    int *inicio= v;
+    int *fim= v+n-1;
+    int temp;
+    while(inicio<fim) {
+        temp= *inicio;
+        *inicio= *fim;
+        *fim= temp;
+        inicio++;
+        fim--;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int v[n];
+    for(int i=0; i<n; i++) scanf("%d", &v[i]);
+    inverterVetor(v, n);
+    for(int i=0; i<n; i++) printf("%d ", v[i]);
+    return 0;
+}

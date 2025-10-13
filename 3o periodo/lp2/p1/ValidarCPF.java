02e8c5e4d6343c8b014f9b75b8c925fceb290d86
ninyjuu <juliabansemer@gmail.com>
2025-10-12 22:13:44 -0300

    a

--- 3o periodo/lp2/p1/ValidarCPF.java
+++ 3o periodo/lp2/p1/ValidarCPF.java
@@ -1,5 +1,74 @@
-public class ValidarCPF {
-    public String cpf;
+import java.util.Scanner;
 
-    public CPF
+public class CPF {
+    public static void main(String[] args){
+        String cpf = lerCPF();
+        if (isCPF(cpf)) {
+            System.out.println("CPF valido");
+        } else {
+            System.out.println("CPF inválido, tente novamente");
+        }
+    }
+
+    public static String lerCPF(){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Digite o CPF: ");
+        String cpfDigitado= scanner.nextLine();
+        return cpfDigitado.replaceAll("\\D", "");
+        scanner.close()
+    }
+
+    public static boolean isCPF(String cpf){
+        if (cpf.equals("00000000000") || cpf.equals("11111111111") ||
+        cpf.equals("22222222222") || cpf.equals("33333333333") ||
+        cpf.equals("44444444444") || cpf.equals("55555555555") ||
+        cpf.equals("66666666666") || cpf.equals("77777777777") ||
+        cpf.equals("88888888888") || cpf.equals("99999999999") ||
+        (cpf.length() != 11))
+        return(false);
+
+        try{
+            int peso= 10;
+            int sm= 0;
+            int num, dig10, dig11;
+
+            for (int i=0; i<9; i++) {
+                num= cpf.charAt(i)- '0';
+                sm= sm+ (num*peso);
+                peso= peso-1;
+            }
+
+            r= 11- (sm % 11);
+            if (r==10 || r==11){
+                dig10= '0';
+            } else dig10= (char)(r+'0');
+
+            peso= 11;
+            for (int i=0; i<10; i++) {
+                num= cpf.charAt(i)- '0';
+                sm= sm+ (num*peso);
+                peso= peso-1;
+            }
+
+            r= 11- (sm % 11);
+            if (r==10 || r==11){
+                dig11= '0';
+            } else dig11= (char)(r+'0');
+
+            if ((dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10)))
+                 return(true);
+            else return(false);
+
+        } catch (Exception erro) {
+            return(false);
+        }
+    }
+
+    public static String imprimeCPF(String cpf) {
+        return(cpf.substring(0,3)+ "." + cpf.substring(3,6)+ "." +
+        cpf.substring(6,9)+ "-" + cpf.substring(9,11));
+    }
 }
+
+
+

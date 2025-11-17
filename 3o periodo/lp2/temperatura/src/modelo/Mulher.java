234229f135f5f24526e9add6898993f230f054c2
ninyjuu <140440526+ninyjuu@users.noreply.github.com>
2025-11-17 00:34:56 -0300

    Add Mulher class extending PessoaIMC

---
+++ 3o periodo/lp2/temperatura/src/modelo/Mulher.java
@@ -0,0 +1,38 @@
+import java.util.GregorianCalendar;
+
+public class Mulher extends PessoaIMC {
+    private String tipoCabelo;
+
+    public Mulher(String nome, String sobreNome, GregorianCalendar dataNasc, long numCPF, double peso, double altura, String tipoCabelo) {
+        super(nome, sobreNome, dataNasc, numCPF, peso, altura);
+        this.tipoCabelo = tipoCabelo;
+    }
+
+    public Mulher(String nome, String sobreNome) {
+        super(nome, sobreNome);
+        this.tipoCabelo = "não informado";
+    }
+
+    public String resultIMC() {
+        float imc = calculaIMC();
+        if (imc < 19.0) return "abaixo do peso ideal";
+        else if (imc <= 25.8) return "peso ideal";
+        else return "acima do peso ideal";
+    }
+
+    public String getTipoCabelo() {
+        return tipoCabelo;
+    }
+
+    public void setTipoCabelo(String tipoCabelo) {
+        if (tipoCabelo != null && !tipoCabelo.trim().isEmpty()) {
+            this.tipoCabelo = tipoCabelo;
+        } else {
+            System.out.println("tipo de cabelo invalido");
+        }
+    }
+
+    public String toString() {
+        return "gênero: feminino\n" + super.toString() + "\ntipo Cabelo: " + this.tipoCabelo;
+    }
+}
